1e91793ffda5d05ea8f9613bbe0c4cb54377f260
Mridun Gupta <gmridun@gmail.com>
2024-01-09 04:49:39 +0530

    final v2 / 2024 edition

--- main.js
+++ main.js
@@ -44,6 +44,32 @@ function updateशायरी() {
 
 document.addEventListener("DOMContentLoaded", updateशायरी);
 
+/* Fetch specialEvents */
+document.addEventListener("DOMContentLoaded", function () {
+    const eventsDisplay = document.getElementById("special-events");
+    const eventsList = [
+        "on the smooth landing of LVM3-M4/Chandrayaan-3",
+        "on the smooth positioning of PSLV-C57/Aditya L1",
+        "for the fastest 5G rollout globally",
+        "on the inauguration of the world's largest meditation center - 'Swarved Mahamandir',",
+        "on the inauguration of the world's longest river cruise - 'MG Ganga Vilas',",
+        "for being the world's fastest-growing major economy,",
+        "for being at the top in digital payments globally,",
+        "on the inauguration of the world's largest office building - 'Surat Diamond Bourse'"
+    ];
+
+    let eventIndex = 0;
+
+    function updateEvent() {
+        eventsDisplay.textContent = eventsList[eventIndex];
+        eventIndex = (eventIndex + 1) % eventsList.length;
+    }
+
+    updateEvent();
+
+    setInterval(updateEvent, 25000);
+});
+
 /* redirectWeb Logic */
 function redirectWeb() {
     var currentDomain = window.location.hostname;
@@ -71,30 +97,4 @@ function redirectWeb() {
     } else {
         console.log("Sorry! This isn't the real website run by 'Mridun Gupta'. Double check the official website for authenticity.");
     }
-}
-
-/* Fetch specialEvents */
-document.addEventListener("DOMContentLoaded", function () {
-    const eventsDisplay = document.getElementById("special-events");
-    const eventsList = [
-        "on the smooth landing of LVM3-M4/Chandrayaan-3",
-        "on the smooth positioning of PSLV-C57/Aditya L1",
-        "for the fastest 5G rollout globally",
-        "on the inauguration of the world's largest meditation center - 'Swarved Mahamandir',",
-        "on the inauguration of the world's longest river cruise - 'MG Ganga Vilas',",
-        "for being the world's fastest-growing major economy,",
-        "for being at the top in digital payments globally,",
-        "on the inauguration of the world's largest office building - 'Surat Diamond Bourse'"
-    ];
-
-    let eventIndex = 0;
-
-    function updateEvent() {
-        eventsDisplay.textContent = eventsList[eventIndex];
-        eventIndex = (eventIndex + 1) % eventsList.length;
-    }
-
-    updateEvent();
-
-    setInterval(updateEvent, 25000);
-})
+};
